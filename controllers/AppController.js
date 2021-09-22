d00e6769932bfd9c05b0e6706f41fa1303334e38
= <=>
2021-09-22 19:30:33 +0200

    completed the post new user method

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,11 +3,11 @@ import RedisClient from '../utils/redis';
 import DBClient from '../utils/db';
 
 class AppController {
-  getStatus(req, res) {
+  static getStatus(req, res) {
     return res.status(200).send({ redis: RedisClient.isAlive(), db: DBClient.isAlive() });
   }
 
-  async getStats(req, res) {
+  static async getStats(req, res) {
     const users = await DBClient.nbUsers();
     const files = await DBClient.nbFiles();
 
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,22 @@
 /* eslint-disable */
+import sha1 from 'sha1';
+import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
 
 class UsersController{
-    postNew() {
+    static async postNew(req, res) {
+        if (!req.body.email) return res.status(400).send({ error: 'Missing email' });
 
+        if (!req.body.password) return res.status(400).send({ error: 'Missing password'});
+
+        const ExistingEmail = DBClient.database.collection('users').findOne({ email: req.body.email });
+
+        if (ExistingEmail) return res.status(400).send({ error: 'Already exist'});
+
+        const securedPassword = sha1(req.body.password);
+
+        await DBClient.database.collection('users').insertOne({ email: req.body.email, password: securedPassword });
+        return response.status(201).send({ id: result.insertedId, email: req.body.email });
     }
 }
 
