2ada795f029498667e64c26730072871f0e561e3
= <=>
2021-09-22 18:51:39 +0200

    inititate database

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,36 @@
 /* eslint-disable */
 const { MongoClient } = require('mongodb');
 
-class DBClient {
+const Host = process.env.DB_HOST || 'localhost';
+const Port = process.env.DB_PORT || 27017;
+const Database = process.env.DB_DATABASE || 'files_manager'; 
 
+class DBClient {
+    constructor() {
+        const url = `mongodb://${Host}:${Port}`;
+        this.client = new MongoClient(url);
+
+        this.client.connect((error) => {
+            error ? this.database = null : this.database = client.db(Database);
+        });   
+    };
+
+    isAlive() {
+        let status;
+        this.database ?  status =  true : status = false;
+        return status;
+    };
+
+    async nbUsers() {
+        const findResult = await this.database.collection('users').find({}).toArray();
+        return findResult.length;
+    };
+
+    async nbFiles() {
+        const findResult = await this.database.collection('files').find({}).toArray();
+        return findResult.length;
+    };
 };
+
+const Client = new DBClient();
+export default Client;
